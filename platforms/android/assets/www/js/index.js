60f5859da9d956425b6959bf46998f123bc230e0
progenitura <iulian.bojinca@gmail.com>
2014-10-18 18:22:52 +0300

    weird working commit

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,51 +1,59 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function init() {
+document.addEventListener("deviceready", deviceReady, true);
+delete init;
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+function checkPreAuth() {
+    var form = $("#loginForm");
+    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
+        $("#username", form).val(window.localStorage["username"]);
+        $("#password", form).val(window.localStorage["password"]);
+        handleLogin();
     }
-};
+}
 
-app.initialize();
+function handleLogin() {
+    var form = $("#loginForm");    
+    //disable the button so we can't resubmit while we wait
+    $("#submitButton",form).attr("disabled","disabled");
+    var u = $("#username", form).val();
+    var p = $("#password", form).val();
+    if(u != '' && p!= '') {
+        $.ajax({
+        	url:"http://hacktm.mngwebs.com/ajax.php",
+        	type: 'POST',
+        	data: {username:u,password:p}
+        	}).done(function(res) {
+        		console.log(res);
+	            if(res == 'true') {
+	                //store
+	                window.localStorage["username"] = u;
+	                window.localStorage["password"] = p;             
+	                //$.mobile.changePage("some.html");
+	                navigator.notification.alert(
+			            'You are the winner!',  // message
+			            function(){},         // callback
+			            'Game Over',            // title
+			            'Done'                  // buttonName
+			        );
+	            } else {
+	                navigator.notification.alert("Your login failed", function() {});
+	            }
+	            $("#submitButton").removeAttr("disabled");
+	    }).fail(function(){
+	    	navigator.notification.alert("Ajax Failed", function() {});
+	    });
+    } else {
+        //Thanks Igor!
+        navigator.notification.alert("You must enter a username and password", function() {});
+        $("#submitButton").removeAttr("disabled");
+    }
+    return false;
+}
+
+function deviceReady() {
+    
+$("#loginForm").on("submit",handleLogin);
+
+}
